2f2e855e2fbaa4c020757e91cd02932fccee3868
Shreyas Talluri <stalluri10@gmail.com>
2023-07-04 15:26:19 -0500

    Navbar(Working)
    Combination of About and Team page with responsive navbar

---
+++ my-app/src/Components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './Card.css'
+function Card({title, imageURL,body,left,top}){
+    return(
+        <div style={{}} className="card-container">
+
+            <div className="image-container">
+                <img src={imageURL} alt='' />
+            </div>
+            
+            <div className="card-title">
+                <h3>{title}</h3>
+            </div>
+
+            <div className="card-body">
+               <p>{body}</p> 
+            </div>
+            
+        </div>
+        
+    )
+}
+
+export default Card;
