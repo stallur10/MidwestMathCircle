27bd84e511eaa25c5d89af3383dffd9fdcb31c86
Shreyas Talluri <stalluri10@gmail.com>
2023-07-18 17:22:05 -0500

    With updated about us

--- my-app/src/pages/MainPage.jsx
+++ my-app/src/pages/MainPage.jsx
@@ -1,34 +1,42 @@
-import './MainPage.css'
 import Slider from '../Components/Slider';
+import './MainPage.css'
+import * as Unicons from '@iconscout/react-unicons';
 
 function MainPage() {
   return (
-    
-    <div>
-          <div className='bigDiv'>
+    <>
+    <div className='bigDiv'>
+      <div className='intro'>
 
-              <div className='intro'>
-                <h1 className='header'>We Are Midwest Math Circle</h1>
-                <button className='registerBtn'>{"Register Now"}</button>
-              </div>
+        <h1 className='header'>We Are Midwest Math Circle</h1>
+        <button className='registerBtn'>{"Register Now"}</button>
 
-              <Slider className="mySlide"/>
-
-          </div>
-
-
-          <div className='bigDiv reverse'>
-
-            <div className='aboutTextDiv'>
-              <h1>About Us</h1>
-              <h3 className='aboutD'>As Albert Einstein once said, "Education is not the learning of facts but the training of minds to think". At MWMC, we embody this philosophy. 
-              We grew frustrated with the approach schools take to math </h3>
-            </div>
-
-            <img src={require("../Images/download.jpeg")} alt='hi' className="aboutImg"/>
-
-          </div>
+      </div>
+      
+      <Slider className="mySlide"/>
     </div>
+
+    <div className='bigDiv reverse'>
+
+      <div className='aboutTextDiv'>
+        <h1 className='gold'>About Us</h1>
+        <h3 className='gold'>As Albert Einstein once said, "Education is not the learning of facts but the training of minds to think". At MWMC, we embody this philosophy. 
+        We grew frustrated with the approach schools take to math, teaching a variety of concepts,
+        but only enough to solve a few cookie cutter problems. We take the opposite approach, teaching our students so
+        that they can look at a problem they'd never seen before, and break it down to solve it. </h3>
+      </div>
+
+      <img src={require("../Images/download.jpeg")} alt='hi' className="aboutImg"/>
+
+    </div>
+
+    <div className='contact'>
+      <Unicons.UilFacebook size="90" color="#4287f5" />
+      <Unicons.UilLinkedin size="90" color="#4287f5" />
+      <Unicons.UilFastMail size="90" color="#4287f5" />
+    </div>
+    
+    </>
   );
 }
 
