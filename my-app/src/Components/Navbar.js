0f64eda153fb30cae1de449fe753599046deeb56
AnvithKotha <anvith.kotha@gmail.com>
2023-08-01 14:56:29 -0500

    Fixed scroll lock

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {FaBars} from "react-icons/fa"
 import { useRef } from "react";
 import './Navbar.css'
@@ -9,18 +9,28 @@ import logo from '../Images/logo.png'
 function Navbar(){
     const navRef= useRef();
     const [state, setState] = useState(0)
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive_nav")
-        if (state == 0) {
-            setState(1)
-            document.body.style.overflow = "hidden"
-        } else {
+    }
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        if (windowWidth > 768) {
             setState(0)
             document.body.style.overflow = ""
         }
-    }
 
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    });
 
     return(
         <header>
@@ -31,10 +41,27 @@ function Navbar(){
 
             <nav ref={navRef}>
                 {/* <Link to="/about" onClick={showNavbar}>About Us</Link> */}
-                <Link to="/home" onClick={showNavbar}>Home</Link>
-                <Link to="/ourteam" onClick={showNavbar}>Our Team</Link>
+                <Link to="/home" onClick={() => {
+                    showNavbar()
+                    setState(0)
+                    document.body.style.overflow = ""
+                }}>Home</Link>
+                <Link to="/ourteam" onClick={() => {
+                    showNavbar()
+                    setState(0)
+                    document.body.style.overflow = ""
+                }}>Our Team</Link>
             </nav>
-            <button className="nav-btn"  onClick={showNavbar}>
+            <button className="nav-btn"  onClick={() => {
+                showNavbar()
+                if (state == 0) {
+                    setState(1)
+                    document.body.style.overflow = "hidden"
+                } else {
+                    setState(0)
+                    document.body.style.overflow = ""
+                }
+            }}>
                 <FaBars/>
             </button>
         </header>
