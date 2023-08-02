06a18509511558019085dd7e90ec33a2cb0c870f
rishikanchi <rishikanchi@programmer.net>
2023-08-01 21:47:46 -0500

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-import { BrowserRouter } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import OurTeamPage from './pages/OurTeamPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
